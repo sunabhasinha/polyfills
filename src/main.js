793f21c9a891b6b89dbdf01192f124211026d07f
sunabhasinha <mailsunabha@gmail.com>
2024-04-07 12:47:09 +0530

    forEach , concat and at plyfill

--- src/main.js
+++ src/main.js
@@ -138,3 +138,95 @@ Array.prototype.mySort = function (compFn) {
 // ];
 // console.log(list.mySort((a, b) => a.age > b.age));
 // console.log(list.sort((a, b) => a.age - b.age));
+
+Array.prototype.myForEach = function (fn) {
+	if (!this) {
+		throw Error('${this} is not defined');
+	}
+
+	const arr = this;
+	const length = this.length;
+
+	for (let i = 0; i < length; i++) {
+		arr[i] = fn(arr[i]);
+	}
+
+	return arr;
+};
+
+// const list = [1, 2, 3, 4];
+// console.log(
+// 	list.forEach((i) => {
+// 		i * 2;
+// 	})
+// );
+
+// console.log(list);
+
+// console.log(
+// 	list.myForEach((i) => {
+// 		i = i * 2;
+// 	})
+// );
+
+/**
+ * At Polyfill
+ * @param {*} index
+ * @returns the item at that particular index and undefined if index is < -length or  >= length
+ */
+
+Array.prototype.myAt = function (index) {
+	if (!this) {
+		throw Error('${this} is not defined');
+	}
+
+	if (!index) {
+		throw Error(`${index} is not defined`);
+	}
+
+	const arr = this;
+	const length = this.length;
+	if (index < -length || index >= length) {
+		return undefined;
+	}
+	return arr[index];
+};
+
+// const list = [1, 2, 3, 4];
+// console.log(list.myAt(10));
+// console.log(list.at(10));
+
+/**
+ * Concat Polyfill
+ * @param  {...any} args
+ * @returns an shallow copy of array by concatenating if concat has arguments, if not then returns the shallow copy of the array
+ */
+
+Array.prototype.myConcat = function (...args) {
+	if (!this) {
+		throw Error('${this} is not defined');
+	}
+
+	let arr = [...this];
+
+	if (args.length === 0) {
+		return arr;
+	}
+
+	for (let i = 0; i < args.length; i++) {
+		if (Array.isArray(args[i])) {
+			arr.push(...args[i]);
+		} else {
+			arr.push(args[i]);
+		}
+	}
+
+	return arr;
+};
+
+const letters = ['a', 'b', 'c'];
+const two = ['d', 'e'];
+
+const alphaNumeric = letters.myConcat();
+
+console.log(alphaNumeric);
